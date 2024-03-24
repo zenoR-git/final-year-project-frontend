f16b15d509a24fc8feaec1451ff04a42d45ecebb
Ratan <gamrat000z@gmail.com>
2024-03-25 02:11:17 +0530

    improved ui

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ function App() {
     });
   }, []);
 
-  const addComment = () => {
+  const addComment = (e) => {
+    e.preventDefault()
     axios
       .post("http://localhost:3001/comments", {
         commentBody: newComment,
@@ -32,13 +33,21 @@ function App() {
 
   return (
     <>
-    <h1>Type a comment here</h1>
-     <div className="container">
-          <div className="typeSec">
-          <textarea
-            rows="20"
-            cols ="80"
-            placeholder="Comment..."
+    <div className="center-block myStyle1">
+    <section className="content-item" id="comments">
+    <div className="container">   
+      <div className="row">
+            <div className="col-sm-8">   
+                <form>
+                  <h3 className="pull-left">New Comment</h3>
+                  <button type="submit" className="btn btn-normal pull-right" onClick={addComment}> Add Comment</button>
+                    <fieldset>
+                        <div className="row">
+                            <div className="col-sm-3 col-lg-2 hidden-xs">
+                            <img className="img-responsive" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""/>
+                            </div>
+                            <div className="form-group col-xs-12 col-sm-9 col-lg-10">
+                                <textarea  id="message" rows="40" cols ="82" placeholder="Comment..."
             autoComplete="off"
             value={newComment}
             onChange={(event) => {
@@ -46,21 +55,38 @@ function App() {
             }}
           >
           </textarea>
-          </div>
-          <div className="buttonSec">
-          <button onClick={addComment}> Add Comment</button>
-          </div>
+                  </div>
+                        </div>  	
+                    </fieldset>
+                </form>
+                
+                <h3>Comments</h3>
         <div className="listOfComments">
           {comments.map((comment, key) => {
             return (
-              <div key={key} className="comment">
-                {comment.commentBody}
+              <div key={key} className="media">
+              <a className="pull-left" href="#"><img className="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" /></a>
+              <div className="media-body">
+                  <h4 className="media-heading">Anonymous</h4>
+                  <p>{comment.commentBody}</p>
+                  <ul className="list-unstyled list-inline media-detail pull-left">
+                      <li><i className="fa fa-calendar"></i>27/02/2014</li>
+                      <li><i className="fa fa-thumbs-up"></i>13</li>
+                  </ul>
+                  <ul className="list-unstyled list-inline media-detail pull-right">
+                      <li className=""><a href="#">Like</a></li>
+                      <li className=""><a href="#">Reply</a></li>
+                  </ul>
               </div>
+             </div>
             );
           })}
-        </div>
-        </div>
-    </>
+          </div>
+          </div>
+          </div></div>
+          </section>
+          </div>
+          </>
   )
 }
 
